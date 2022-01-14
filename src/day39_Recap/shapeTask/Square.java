8f83c9c86574ee78ab50792b08485307bdce6d56
ruthdgalvan <ruthdgalvan@gmail.com>
2022-01-14 11:49:01 -0500

    day39_Recap subclass

--- src/day39_Recap/shapeTask/Square.java
+++ src/day39_Recap/shapeTask/Square.java
@@ -17,14 +17,27 @@ private double side;
     }
 
     public Square(String name, double side) {
-        super(name);
+        super("Square");
         setSide(side);
     }
 
+    @Override
+    public double area() {
+        return side * side;
+    }
 
+    @Override
+    public double perimeter() {
+        return side * 4;
+    }
 
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
 
-
-
-
+    }
 }
