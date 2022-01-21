786b8379840313fa00c6d0b07cb5972e5351c05b
ruthdgalvan <ruthdgalvan@gmail.com>
2022-01-21 13:11:07 -0500

    day39_Recap subclass

---
+++ src/day41_Exceptions/Checked_vs_Unchecked.java
@@ -0,0 +1,33 @@
+package day41_Exceptions;
+
+public class Checked_vs_Unchecked {
+
+    public static void main(String[] args) {
+
+        // unchecked exceptions:
+        int a = 10;
+        int b = 0;
+
+        //System.out.println( a / b );
+        //System.out.println("Wooden Spoon");
+
+        char[] characters = {'A', 'B', 'C'};
+                    //        0    1    2
+        //System.out.println(characters[99]);
+
+        String str = "Wooden Spoon";
+
+        str = null;
+
+        System.out.println(str.toUpperCase());
+
+
+
+
+
+
+
+
+
+    }
+}
